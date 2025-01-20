ed07c4f5e02397bd07408e18f73fde8c7b62a2f1
IGhifari <rinnitoshii07@gmail.com>
2025-01-20 11:56:34 +0700

    Menambahkan gambar rumah

--- game.js
+++ game.js
@@ -1,34 +1,43 @@
 const player = document.getElementById('player');
+const rumah = document.querySelector('.rumah');
 let x = 0;
 let y = 0;
 const speed = 10;
+
 document.addEventListener('keydown', (e) => {
+    const rumahRect = rumah.getBoundingClientRect();
+    const playerRect = player.getBoundingClientRect();
+
     switch(e.key) {
         case 'ArrowRight':
         case 'd':
         case 'D':
-            if(x + speed + player.width <= window.innerWidth) {
+            if(x + speed + playerRect.width <= window.innerWidth && 
+               !(x + speed + playerRect.width > rumahRect.left && y + playerRect.height > rumahRect.top && y < rumahRect.bottom)) {
                 x += speed;
             }
             break;
         case 'ArrowLeft': 
         case 'a':
         case 'A':
-            if(x - speed >= 0) {
+            if(x - speed >= 0 && 
+               !(x - speed < rumahRect.right && y + playerRect.height > rumahRect.top && y < rumahRect.bottom)) {
                 x -= speed;
             }
             break;
         case 'ArrowUp':
         case 'w':
         case 'W':
-            if(y - speed >= 0) {
+            if(y - speed >= 0 && 
+               !(y - speed < rumahRect.bottom && x + playerRect.width > rumahRect.left && x < rumahRect.right)) {
                 y -= speed;
             }
             break;
         case 'ArrowDown':
         case 's':
         case 'S':
-            if(y + speed + player.height <= window.innerHeight) {
+            if(y + speed + playerRect.height <= window.innerHeight && 
+               !(y + speed + playerRect.height > rumahRect.top && x + playerRect.width > rumahRect.left && x < rumahRect.right)) {
                 y += speed;
             }
             break;
