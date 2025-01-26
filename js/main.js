5c27b614dd4c24b63b2541638017d2b968bdad2d
Zaaaaaae25 <ezafadlan509@gmail.com>
2025-01-26 14:04:50 +0700

    tentang set

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleAbout() {
+  const aboutContainer = document.getElementById("aboutContainer");
+  if (aboutContainer.style.display === "none") {
+    aboutContainer.style.display = "block";
+  } else {
+    aboutContainer.style.display = "none";
+  }
+}
